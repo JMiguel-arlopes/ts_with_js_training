b6f0f59357ca64ceebaf7a9938c70b786cf2e74b
JM-arlopes <jmiguelarlopes@gmail.com>
2024-03-22 23:56:36 -0300

    tirando codigo desnecessário

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState, ChangeEvent, FormEvent } from "react";
 import "./App.css";
-import Input from "./components/Input";
-import Submit from "./components/Submit";
 import Form from "./components/Form";
 
 function App() {
